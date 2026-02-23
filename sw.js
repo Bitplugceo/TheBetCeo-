6a8bcddff684f899a66aab5736a44a0a60d93b05
Bitplugceo <ridwantunde636@gmail.com>
2026-02-23 02:09:30 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,13 +1,31 @@
+const CACHE_NAME = "bb-engine-v2";
+
+const FILES_TO_CACHE = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./0A45F320-A89F-4851-AA4E-CE22138FD52F.jpeg"
+];
+
 self.addEventListener("install", event => {
   event.waitUntil(
-    caches.open("bb-engine-v1").then(cache => {
-      return cache.addAll([
-        "./",
-        "./index.html",
-        "./manifest.json"
-      ]);
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(FILES_TO_CACHE);
     })
   );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.filter(key => key !== CACHE_NAME)
+            .map(key => caches.delete(key))
+      );
+    })
+  );
+  self.clients.claim();
 });
 
 self.addEventListener("fetch", event => {
